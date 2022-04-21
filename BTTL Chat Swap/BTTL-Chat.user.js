fe2acc7e1943b2a768cc58dd447aec66aebf62a8
AnimeshnikMC <47035913+AnimeshnikMC@users.noreply.github.com>
2022-04-21 16:15:58 +0700

    update

---
+++ BTTL Chat Swap/BTTL-Chat.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         BTTL Chat Swap
+// @namespace    https://github.com/AnimeshnikMC/my-mini-scripts/
+// @version      0.4.0
+// @description  Change the chat placement.
+// @author       Animeshnik_Maynkrafter
+// @match        https://trovo.live/*
+// @icon         https://www.google.com/s2/favicons?sz=128&domain=trovo.live
+// @updateURL    https://github.com/AnimeshnikMC/my-mini-scripts/raw/main/BTTV%20Chat%20Layout.user.js
+// @downloadURL  https://github.com/AnimeshnikMC/my-mini-scripts/raw/main/BTTV%20Chat%20Layout.user.js
+// @run-at       document-end
+// @grant        GM_log
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @resource     cx_CSS file:///E:/style.css
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const cx1=GM_getResourceText("cx_CSS");
+    GM_addStyle(cx1);
+})();
