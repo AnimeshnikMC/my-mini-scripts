251358465ab2265223f190cf5727581800bd5ecf
AnimeshnikMC <47035913+AnimeshnikMC@users.noreply.github.com>
2022-05-07 16:09:55 +0700

    fix

--- BTTL Chat Swap/BTTL-Chat.user.js
+++ BTTL Chat Swap/BTTL-Chat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BTTL Chat Swap
 // @namespace    https://github.com/AnimeshnikMC/my-mini-scripts/
-// @version      0.4.1
+// @version      0.4.1.5
 // @description  Change the chat placement.
 // @author       Animeshnik_Maynkrafter
 // @match        https://trovo.live/*
