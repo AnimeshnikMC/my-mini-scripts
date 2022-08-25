0f8c8258d065812ad1c55e85c1fdb9dc7135b591
AnimeshnikMC <47035913+AnimeshnikMC@users.noreply.github.com>
2022-08-25 16:52:28 +0700

    Update BTTL-Chat.user.js

--- BTTL Chat Swap/BTTL-Chat.user.js
+++ BTTL Chat Swap/BTTL-Chat.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         BTTL Chat Swap(dev)
 // @namespace    https://github.com/AnimeshnikMC/my-mini-scripts
-// @version      0.4.2-dev.5
+// @version      0.4.2-dev.6
 // @description  Change the chat placement.
 // @author       Animeshnik_Maynkrafter
 // @match        https://trovo.live/*
 // @icon         https://www.google.com/s2/favicons?sz=128&domain=trovo.live
-// @updateURL    https://github.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/BTTL-Chat.user.js
-// @downloadURL  https://github.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/BTTL-Chat.user.js
+// @updateURL    https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/BTTL-Chat.user.js
+// @downloadURL  https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/BTTL-Chat.user.js
 // @run-at       document-end
 // @grant        GM_log
 // @grant        GM_registerMenuCommand
 // @grant        GM_unregisterMenuCommand
-// @resource     cx_CSS https://github.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/style.css
+// @resource     cx_CSS https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/style.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
