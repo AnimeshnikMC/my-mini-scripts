3b598df91726ac03055bd8246abd518c7c83e38c
AnimeshnikMC <47035913+AnimeshnikMC@users.noreply.github.com>
2022-08-26 16:47:18 +0700

    Update BTTL-Chat.user.js

--- BTTL Chat Swap/BTTL-Chat.user.js
+++ BTTL Chat Swap/BTTL-Chat.user.js
@@ -6,13 +6,13 @@
 // @author       Animeshnik_Maynkrafter
 // @match        https://trovo.live/*
 // @icon         https://www.google.com/s2/favicons?sz=128&domain=trovo.live
-// @updateURL    https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/BTTL-Chat.user.js
-// @downloadURL  https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/BTTL-Chat.user.js
+// @updateURL    https://github.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/BTTL-Chat.user.js
+// @downloadURL  https://github.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/BTTL-Chat.user.js
 // @run-at       document-start
 // @grant        GM_log
 // @grant        GM_registerMenuCommand
 // @grant        GM_unregisterMenuCommand
-// @resource     cx_CSS https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/style.css
+// @resource     cx_CSS https://github.com/AnimeshnikMC/my-mini-scripts/raw/dev/BTTL%20Chat%20Swap/style.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
