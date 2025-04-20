baef75a480b32caf52749d47aea28821c2ec82d1
AnimeshnikMC <AnimeshnikMC@mail.ru>
2025-04-20 13:38:16 +0700

    update 0.5.6 fix firefox error

--- search for anime at shikimori.one/search-for-anime-at-shikimori.one.user.js
+++ search for anime at shikimori.one/search-for-anime-at-shikimori.one.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         search for anime at shikimori.one
 // @namespace    https://github.com/AnimeshnikMC/my-mini-scripts
-// @version      0.5.5
+// @version      0.5.6
 // @description  ...
 // @author       AnimeshnikMC
 // @match        *://*/*
 // @exclude      /^[^:/#?]*:\/\/([^#?/]*\.)?shikimori\.one(:[0-9]{1,5})?\/.*$/
-// @run-at       document-start
+// @run-at       document-end
 // @downloadURL  https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/main/search%20for%20anime%20at%20shikimori.one/search-for-anime-at-shikimori.one.user.js
 // @updateURL    https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/main/search%20for%20anime%20at%20shikimori.one/search-for-anime-at-shikimori.one.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shikimori.one
@@ -28,15 +28,15 @@
         if(Private===0){GM_openInTab(p0?.animes[0]?.url,{loadInBackground:true})}
         else if(Private===1){GM_openInTab(p0?.animes[0]?.url,{incognito:true})}
     }
-    function s0(){var p0;p0=window.getSelection();return p0.toString()}
-    setInterval(()=>{
+    function s0(){var p0=window.getSelection();return p0.toString()}
+    document.addEventListener("selectionchange",(event)=>{
         var p0=s0();
-       if(p0.length){
-           btn1=GM_registerMenuCommand(`search ${p0}`,()=>{if(p0.length)getAnime(p0)},{id:"btn01",title:`${p0}`})
-           btn2=GM_registerMenuCommand(`search private ${p0}`,()=>{if(p0.length)getAnime(p0,1)},{id:"btn02",title:`${p0}`})
-       }
-       if(!p0.length&&btn1!=="off"){GM_unregisterMenuCommand(btn1);btn1="off"}
-       if(!p0.length&&btn2!=="off"){GM_unregisterMenuCommand(btn2);btn2="off"}
-    },1000)
+        if(p0.length){
+            btn1=GM_registerMenuCommand(`search ${p0}`,()=>{if(p0.length)getAnime(p0)},{id:"btn01",title:`${p0}`})
+            btn2=GM_registerMenuCommand(`search private ${p0}`,()=>{if(p0.length)getAnime(p0,1)},{id:"btn02",title:`${p0}`})
+        }
+        if(!p0.length&&btn1!=="off"){GM_unregisterMenuCommand(btn1);btn1="off"}
+        if(!p0.length&&btn2!=="off"){GM_unregisterMenuCommand(btn2);btn2="off"}
+    })
 
 })();
