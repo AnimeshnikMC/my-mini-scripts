9fe78d8d320bf4bc353fcc94866c9a01bfda88f5
AnimeshnikMC <AnimeshnikMC@mail.ru>
2026-04-23 11:09:46 +0300

    Update 0.6.0: added a multi-search for a site based on shikimori.one

--- search for anime at shikimori.one/search-for-anime-at-shikimori.one.user.js
+++ search for anime at shikimori.one/search-for-anime-at-shikimori.one.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         search for anime at shikimori.one
+// @name         search for anime at shikimori
 // @namespace    https://github.com/AnimeshnikMC/my-mini-scripts
-// @version      0.5.6
-// @description  ...
+// @version      0.6.0
+// @description  Update 0.6.0: added a multi-search for a site based on shikimori.one
 // @author       AnimeshnikMC
 // @match        *://*/*
-// @exclude      /^[^:/#?]*:\/\/([^#?/]*\.)?shikimori\.one(:[0-9]{1,5})?\/.*$/
+// @exclude      /^[^:/#?]*:\/\/([^#?/]*\.)?shikimori\.fi(:[0-9]{1,5})?\/.*$/
+// @exclude      /^[^:/#?]*:\/\/([^#?/]*\.)?shikimori\.io(:[0-9]{1,5})?\/.*$/
 // @run-at       document-end
 // @downloadURL  https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/main/search%20for%20anime%20at%20shikimori.one/search-for-anime-at-shikimori.one.user.js
 // @updateURL    https://raw.githubusercontent.com/AnimeshnikMC/my-mini-scripts/main/search%20for%20anime%20at%20shikimori.one/search-for-anime-at-shikimori.one.user.js
@@ -19,26 +20,63 @@
 
 (function(){
 	'use strict';
-
-	var btn1="off",btn2="off";
-	async function getAnime(title,Private=0){
-		var url="https://shikimori.one/api/graphql",
+	var host=['shikimori.fi','shikimori.io'],genBtn=new genBtn;
+	async function getAnime(title,host){
+		var url=`https://${host}/api/graphql`,
 			cfg={method:"POST",headers:{"Content-Type":"application/json"},
 				body:JSON.stringify({query:`{animes(search:"${title}",limit:1,kind:"!special,!tv_special"){id,malId,name,russian,url,kind}}`})
 			},p0;
 		p0=await fetch(url,cfg).then(res=>res.json()).then(data=>data.data);
-		if(Private===0){GM_openInTab(p0?.animes[0]?.url,{loadInBackground:true})}
-		else if(Private===1){GM_openInTab(p0?.animes[0]?.url,{incognito:true})}
+		return p0?.animes[0]?.url;
 	}
 	function s0(){var p0=window.getSelection();return p0.toString()}
+	function genBtn(){
+		var btnList={},
+			OK=(object)=>{return Object.keys(object)};
+		this.createBtn=(p0,title)=>{
+			var btnN=0
+			for(let i=0;i<p0.length;i++){
+				const p1=[
+					[
+						`[${p0[i]}] search ${title}`,
+						async()=>{
+							let p=await getAnime(title,p0[i]);
+							p.length?GM_openInTab(p,{loadInBackground:true}):alert(`not found "${title}"`)
+						},
+						{id:`btn${btnN+1}`,title:`${title}`}
+					],
+					[
+						`[${p0[i]}] search private ${title}`,
+						async()=>{
+							let p=await getAnime(title,p0[i]);
+							p.length?GM_openInTab(p,{incognito:true}):alert(`not found "${title}"`)
+						},
+						{id:`btn${btnN+2}`,title:`${title}`}
+					]
+				];
+				btnList[`btn${btnN+1}`]=GM_registerMenuCommand(...p1[0]);
+				btnList[`btn${btnN+2}`]=GM_registerMenuCommand(...p1[1]);
+				btnN=btnN+2
+			}
+			//CL(btnList)
+		}
+		this.removeBtn=()=>{
+			var p0=OK(btnList)
+			for(let i=0;i<p0.length;i++){
+				const e=p0[i];
+				GM_unregisterMenuCommand(btnList[e]);
+				delete btnList[e];
+			};
+			//CL(btnList)
+		}
+	}
 	document.addEventListener("selectionchange",(event)=>{
 		var p0=s0();
 		if(p0.length){
-			btn1=GM_registerMenuCommand(`search ${p0}`,()=>{if(p0.length)getAnime(p0)},{id:"btn01",title:`${p0}`})
-			btn2=GM_registerMenuCommand(`search private ${p0}`,()=>{if(p0.length)getAnime(p0,1)},{id:"btn02",title:`${p0}`})
+			genBtn.createBtn(host,p0)
+		}else{
+			genBtn.removeBtn()
 		}
-		if(!p0.length&&btn1!=="off"){GM_unregisterMenuCommand(btn1);btn1="off"}
-		if(!p0.length&&btn2!=="off"){GM_unregisterMenuCommand(btn2);btn2="off"}
 	})
 
 })();
